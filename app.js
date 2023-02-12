47b9844f8810052053954789a248665b2799b391
Frisy Novel <98008631+freezyyyyyy@users.noreply.github.com>
2023-02-12 14:40:53 +0700

    hehe

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
-const asdf = {
-    title: 'HEyz', 
-    turb: 'Mongoose',
-    message: '54Ya 4lAyyy 131ngitzzz',
-    paragraph: 'zaya k4muuu zayankkkk'
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    mongoose.set('strictQuery', true)
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const silence = new Kitten({ name: 'Hamurrabi' });
+    console.log(silence.name);
+
+    const asdf = {
+        title: 'HEyz',
+        turb: 'Mongoose',
+        message: '54Ya 4lAyyy 131ngitzzz',
+        paragraph: 'zaya k4muuu zayankkkk'
+    }
+
+    app.get('/', (req, res) => {
+        res.render('index', asdf);
+    })
+
+    app.listen(port, () => {
+        console.log(`Listening ${port}`);
+    })
 }
 
-app.get('/', (req, res) => {
-    res.render('index', asdf);    
-})
 
-app.listen(port, ()=> {
-    console.log(`Listening ${port}`);
-})
